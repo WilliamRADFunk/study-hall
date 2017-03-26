66892894ac7208431583cdfdbda8a127eca55b2d
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-03-26 17:03:25 -0400

    Isolates events map to events list page.

--- js/controllers.js
+++ js/controllers.js
@@ -47,22 +47,15 @@ studyHallApp.controller('LoginController', ['appData', function(app) {
 // Main function is manage event lists "page".
 studyHallApp.controller('EventsController', ['appData', function(app) {
 	var self = this;
-	// var mymap = L.map('mapid').setView([28.6024, 81.2001], 2);
-	// L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	// 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	// 	maxZoom: 18,
-	// 	id: 'your.mapbox.project.id',
-	// 	accessToken: 'your.mapbox.public.access.token'
-	// }).addTo(mymap);
 
-	var map = L.map('map').setView([51.505, -0.09], 13);
+	var map = L.map('map-events').setView([28.6024, -81.2001], 16);
 
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 
-	L.marker([51.5, -0.09]).addTo(map)
-	    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+	L.marker([28.6024, -81.2001]).addTo(map)
+	    .bindPopup('University of<br>Central Florida')
 	    .openPopup();
 
 	self.active = false;

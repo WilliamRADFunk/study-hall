7bc0bfa0302eb2866ba408e7390afa402aad64ce
William RAD Funk <contact@WILLIAMRADFunk.com>
2017-04-08 21:10:49 -0400

    Adds some styling and minor functionality to the rso group page.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 var studyHallApp = angular.module('studyHallApp', []);
+
+// Filters to make all uppercase.
+studyHallApp.filter("break", function($sce){
+  return function(value){
+    if (!angular.isString(value)) return value;
+    return $sce.trustAsHtml(value.toUpperCase());
+  };
+});
